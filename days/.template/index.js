4c3662109d0c715d6d106bfb7443f96b001544ae
Alex Rieger <alex@rieger.sh>
2023-12-01 08:47:01 +0100

    Create index.js

---
+++ days/.template/index.js
@@ -0,0 +1,13 @@
+const silver = (input, {}) => {
+    console.log('not solved', input)
+    return null
+}
+
+const gold = (input, {}) => {
+    console.log('not solved')
+    return null
+}
+
+export default {
+    silver, gold
+}
